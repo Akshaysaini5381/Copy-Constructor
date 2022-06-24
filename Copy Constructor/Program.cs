1898c63b0716889a85c80fa68bd0b60ba796fe01
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-24 05:57:43 +0530

    Add project files.

---
+++ Copy Constructor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace test_program
+{
+
+    class Employee
+    {
+        private int empno;
+        private string empname;
+        public Employee(int Empno, string Empname)
+        {
+            this.empno = Empno;
+            this.empname = Empname;
+        }
+        public Employee(Employee E1)
+        {
+            this.empno = E1.empno;
+            this.empname = E1.empname;
+        }
+        public void display()
+        {
+            Console.WriteLine("Copy Constructor :"+empno + " " + empname);
+        }
+    }
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee(10, "akshay");
+            // copy obj
+             Employee obj2 = new Employee(obj);
+            obj.display();
+            obj2.display();
+
+        }
+    }
+}
+
